344eabce323b7a7b02dd3ca70927c8c3c1b89082
meretska-sofiia <meretska.sofiia@gmail.com>
2022-11-02 19:47:30 +0200

    Auth page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,5 @@
+import AuthPage from 'pages/AuthPage/AuthPage';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        fontFamily: 'poppins_bold',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template upd
-    </div>
-  );
+  return <AuthPage />;
 };
