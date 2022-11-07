a1d6e18e248f9b7abbe3c2148e32e5b2c68074b1
OstapenkoIvan <75904652+OstapenkoIvan@users.noreply.github.com>
2022-11-07 22:53:27 +0000

    Deploying to gh-pages from @ OstapenkoIvan/wallet-app@80af85ac7e76cfddaa9babbaa4489a3b3f274998 🚀

---
+++ static/js/964.45d41054.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwallet_app=self.webpackChunkwallet_app||[]).push([[964],{4964:function(e,n,a){a.r(n),a.d(n,{default:function(){return o}});var r=a(4925),t=a(2791),l=a(9299),s=a(7689),u=a(9434),c=a(2507),d=a(3329),i=["redirectTo","end"];function o(e){var n=e.redirectTo;e.end,(0,r.Z)(e,i);return(0,u.v9)(c.Od)?(0,d.jsx)(s.Fg,{to:n}):(0,d.jsx)(t.Suspense,{fallback:(0,d.jsx)(l.Z,{isLoading:!0,global:!0}),children:(0,d.jsx)(s.j3,{})})}}}]);
+//# sourceMappingURL=964.45d41054.chunk.js.map
